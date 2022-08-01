c8a288d20f35ddfa3162a780a16875a982815aff
Sushil <mauryasushil045@gmail.com>
2022-08-01 21:49:04 +0530

    Getting: Requiring module node_modules\react-native-reanimated\src\Animated.js error on running DrawerNavi

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
-import { NavigationContainer } from "@react-navigation/native";
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
-import navigationTheme from './app/navigation/navigationTheme';
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    </View>
+  );
+}
 
-import AppNavigator from './app/navigation/AppNavigator';
-import AuthNavigator from './app/navigation/AuthNavigator';
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer theme={navigationTheme} >
-        <AuthNavigator />
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
--- app/configs/colors.js
+++ app/configs/colors.js
@@ -6,5 +6,9 @@ export default {
     medium: '#6e6969',
     light: '#f8f4f4',
     dark: '#0c0c0c',
-    danger: '#ff5252'
+    danger: '#ff5252',
+
+// added from youtube color palate
+    darkPurple: "#20315f",
+    purple: "#ad40af",
 }
